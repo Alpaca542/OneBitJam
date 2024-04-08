70f206d2514797ae63aac49526ab8f84475393bb
Alpaca <mihaililyin08@gmail.com>
2024-04-08 12:22:10 +0400

    boxfix

--- Assets/Scripts/Forcer.cs
+++ Assets/Scripts/Forcer.cs
@@ -7,11 +7,14 @@ public class Forcer : MonoBehaviour
     public float Strength = 1500f;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.TransformVector(new Vector2(1000, 1000)));
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.tag == "Player" || collision.gameObject.tag == "Box")
         {
-            collision.gameObject.GetComponent<Player>().AmIFlying = true;
+            if (collision.gameObject.tag == "Player")
+            {
+                collision.gameObject.GetComponent<Player>().AmIFlying = true;
+            }
+
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * Strength);
         }
-        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * Strength);
     }
 }

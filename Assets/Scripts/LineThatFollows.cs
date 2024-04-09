be35756e64a62bf5443f1259f03847509405a6b8
Alpaca <mihaililyin08@gmail.com>
2024-04-09 13:20:00 +0400

    Update LineThatFollows.cs

--- Assets/Scripts/LineThatFollows.cs
+++ Assets/Scripts/LineThatFollows.cs
@@ -14,6 +14,6 @@ public class LineThatFollows : MonoBehaviour
     void Update()
     {
         lr.SetPosition(0, GameObject.FindGameObjectWithTag("Slime").transform.position);
-        lr.SetPosition(1, WhatToFollow.transform.position);
+        lr.SetPosition(1, new Vector2(WhatToFollow.transform.position.x, WhatToFollow.transform.position.y+0.72f));
     }
 }

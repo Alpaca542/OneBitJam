435361788e2d920391bd379ab81f21a5d28117b8
Alpaca <mihaililyin08@gmail.com>
2024-04-13 20:57:55 +0400

    PostProcessing(WARNING: I'm not sure that i did everything correctly, so I made a backup branch)

--- Assets/Scripts/Stretchers.cs
+++ Assets/Scripts/Stretchers.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class Stretchers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public IEnumerator Openpanel()
     {
-        
-    }
+        if (transform.localScale != new Vector3(1, 1, 1))
+        {
+            transform.localScale = new Vector2(0f, 0f);
+            yield return new WaitForSeconds(0.02f);
+            transform.localScale = new Vector2(0.2f, 0.2f);
+            yield return new WaitForSeconds(0.02f);
+            transform.localScale = new Vector2(0.4f, 0.4f);
+            yield return new WaitForSeconds(0.02f);
+            transform.localScale = new Vector2(0.6f, 0.6f);
+            yield return new WaitForSeconds(0.02f);
+            transform.localScale = new Vector2(0.8f, 0.8f);
+            yield return new WaitForSeconds(0.02f);
+            transform.localScale = new Vector2(1, 1);
 
-    // Update is called once per frame
-    void Update()
+        }
+    }
+    public IEnumerator CLosepanel()
     {
-        
+        if (transform.localScale != new Vector3(0, 0, 1))
+        {
+            transform.localScale = new Vector2(1, 1);
+            yield return new WaitForSeconds(0.02f);
+            transform.localScale = new Vector2(0.8f, 0.8f);
+            yield return new WaitForSeconds(0.02f);
+            transform.localScale = new Vector2(0.6f, 0.6f);
+            yield return new WaitForSeconds(0.02f);
+            transform.localScale = new Vector2(0.4f, 0.4f);
+            yield return new WaitForSeconds(0.02f);
+            transform.localScale = new Vector2(0.2f, 0.2f);
+            yield return new WaitForSeconds(0.02f);
+            transform.localScale = new Vector2(0, 0);
+        }
     }
 }

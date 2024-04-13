104ae00f34c9dc38198c16c5912ddcba22c0d7ca
Alpaca <mihaililyin08@gmail.com>
2024-04-13 13:36:27 +0400

    BOBOOOOH

--- Assets/Scripts/LevelFinisher.cs
+++ Assets/Scripts/LevelFinisher.cs
@@ -9,7 +9,30 @@ public class LevelFinisher : MonoBehaviour
     public GameObject panelForPause;
     public DialogueScript dlgmng;
     public GameObject SmokeEffect;
+    public GameObject explosion1;
     public void StopTheLevel()
+    {
+        if(SceneManager.GetActiveScene().name != "Lvl1" && SceneManager.GetActiveScene().name != "Lvl6")
+        {
+            GameObject.FindGameObjectWithTag("Slime").SetActive(false);
+            explosion1.SetActive(true);
+            if (SceneManager.GetActiveScene().name == "Lvl2" || SceneManager.GetActiveScene().name == "Lvl3")
+                Camera.main.GetComponent<Camera>().orthographicSize = 10f;
+            if (SceneManager.GetActiveScene().name == "Lvl4")
+                Camera.main.GetComponent<Camera>().orthographicSize = 15f;
+            if (SceneManager.GetActiveScene().name == "Lvl4")
+                Camera.main.GetComponent<Camera>().orthographicSize = 20f;
+            Camera.main.GetComponent<playerFollow>().enabled = false;
+            Camera.main.transform.position = new Vector3(explosion1.transform.position.x, explosion1.transform.position.y, -10);
+
+            Invoke(nameof(StopTheLevelInvoke), 0.8f);
+        }
+        else
+        {
+            StopTheLevelInvoke();
+        }
+    }
+    public void StopTheLevelInvoke()
     {
         panelForTheEnd.SetActive(true);
         PlayerPrefs.SetInt("CurentLevel", System.Convert.ToInt32(SceneManager.GetActiveScene().name.Replace("Lvl", "")));

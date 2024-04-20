d5e05640a619cf1ebd13fe6ce15166b7532975a7
Alpaca <mihaililyin08@gmail.com>
2024-04-20 13:40:53 +0400

    CoolMenu

--- Assets/Scripts/LineThatFollows.cs
+++ Assets/Scripts/LineThatFollows.cs
@@ -1,25 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
+using UnityEngine.UI;
 
 public class LineThatFollows : MonoBehaviour
 {
     public GameObject WhatToFollow;
     public LineRenderer lr;
+    public bool AmISilent;
+    public RectTransform cnv;
     public soundManager soundManager;
     public GameObject sounder;
     private void Start()
     {
-        soundManager = GetComponent<soundManager>();
-        gameObject.GetComponent<soundManager>().PlaySound(0);
+        if (!AmISilent)
+        {
+            soundManager = GetComponent<soundManager>();
+            gameObject.GetComponent<soundManager>().PlaySound(0);
+        }
+        lr = GetComponent<LineRenderer>();
         lr.positionCount = 2;
     }
     void Update()
     {
 
         lr.SetPosition(0, GameObject.FindGameObjectWithTag("Slime").transform.position);
-        lr.SetPosition(1, new Vector2(WhatToFollow.transform.position.x, WhatToFollow.transform.position.y+0.72f));
+        if (!AmISilent)
+        {
+            lr.SetPosition(1, new Vector2(WhatToFollow.transform.position.x, WhatToFollow.transform.position.y + 0.72f));
+        }
+        else
+        {
+            lr.SetPosition(1, new Vector2(WhatToFollow.transform.position.x, WhatToFollow.transform.position.y));
+        }
     }
     private void OnDestroy()
     {

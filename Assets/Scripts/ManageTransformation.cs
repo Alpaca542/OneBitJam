b2c3fd48c561fb26dc6ccc2042c99aa2cdb6b124
Alpaca <mihaililyin08@gmail.com>
2024-04-25 13:21:33 +0400

    BetterSwitch

--- Assets/Scripts/ManageTransformation.cs
+++ Assets/Scripts/ManageTransformation.cs
@@ -10,17 +10,25 @@ public class ManageTransformation : MonoBehaviour
     public GameObject[] playerList;
     public GameObject lightParticles;
     public GameObject[] BtnList;
-    public RectTransform panel;
+    public GameObject[] ObjList;
     public GameObject slime;
     public GameObject line;
     public GameObject ln;
+    public GameObject TransformCanvas;
     Coroutine crtn;
+    public GameObject slimeBtn;
     public LayerMask defaultlayer;
     public LayerMask playerlayer;
     public void OnChosenInPanel(int BtnNumber)
     {
         if (Camera.main.GetComponent<playerFollow>().player != playerList[BtnNumber])
         {
+            foreach (GameObject gmb in BtnList)
+            {
+                gmb.GetComponent<Image>().color = new Color32(255, 255, 255, Convert.ToByte(0.4f * 255));
+            }
+            slimeBtn.GetComponent<Image>().color = new Color32(255, 255, 255, Convert.ToByte(0.4f * 255));
+            BtnList[BtnNumber].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
             if (ln != null)
                 Destroy(ln);
             foreach (GameObject gmb in playerList)
@@ -65,6 +73,11 @@ public class ManageTransformation : MonoBehaviour
     }
     public void OnChosenSlimel()
     {
+        foreach (GameObject gmb in BtnList)
+        {
+            gmb.GetComponent<Image>().color = new Color32(255, 255, 255, Convert.ToByte(0.3f * 255));
+        }
+        slimeBtn.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
         if (Camera.main.GetComponent<playerFollow>().player.tag != "Slime")
         {
             Transform pltr = Camera.main.GetComponent<playerFollow>().player.transform;
@@ -85,10 +98,30 @@ public class ManageTransformation : MonoBehaviour
         slime = GameObject.FindGameObjectWithTag("Slime");
         playerList = GameObject.FindGameObjectsWithTag("Player");
         for (int i = 0; i < playerList.Length; i++)
+        {
+            ObjList[i].SetActive(true);
+        }
+    }
+    private void Update()
     {
-            BtnList[i].SetActive(true);
-            BtnList[i].GetComponent<Image>().sprite = playerList[i].GetComponent<SpriteRenderer>().sprite;
-            BtnList[i].GetComponent<Image>().color = playerList[i].GetComponent<SpriteRenderer>().color;
+        if (TransformCanvas.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1) && ObjList[0].activeSelf)
+            {
+                OnChosenInPanel(0);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2) && ObjList[1].activeSelf)
+            {
+                OnChosenInPanel(1);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3) && ObjList[2].activeSelf)
+            {
+                OnChosenInPanel(2);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha4) && ObjList[3].activeSelf)
+            {
+                OnChosenInPanel(3);
+            }
         }
     }
 }
